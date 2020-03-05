152594156cd356440a7880e6d0568addcdaf96db
carlijnbruin <carlijn.bruin@hva.nl>
2020-03-05 21:38:26 +0100

    backend goed werkend gemaakt, behalve de css en responsive nog

--- week_1/myapp/index.js
+++ week_1/myapp/index.js
@@ -1,52 +1,229 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const slug = require('slug');
 const app = express();
 const port = 3000;
 
+let users = [];
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 app.use('/static', express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.write('<p>Hello World!<p>');
-  res.end();
-});
 
-app.get('/about', (req, res) => {
-  res.write('<p>It\'s allllll about me!<p>');
-  res.end();
-});
+app.get('/', function(req, res){
+  res.redirect('/aanmelden')
+})
 
+//GET to request data from a document
+app.get('/aanmelden', function(req, res){
+  res.render('aanmelden.ejs');
+})
+
+//GET, to request data from a document
 app.get('/voornaam/:id', function(req, res){
-  res.send('My name is ' +req.params.id);
+  const id = req.params.id; // ? var id opslaan
+  const user = users.filter(user => user.id === id)[0];
+  res.render('voornaam.ejs', user); // twee parameters toegeven. de pagina en de juiste user die is gekozen.
 })
 
-//Dit is een route parameter
-app.get('/achternaam/:id', function(req, res){
-  res.send('My last name is ' +req.params.id);
+//GET, to request data from a document
+app.get('/geboortedatum/:id', function(req, res){
+  const id = req.params.id; //var id opslaan
+  const user = users.filter(user => user.id === id)[0] // kijken of id overeenkomt met de id hierboven.
+  res.render('geboortedatum.ejs', user);
 })
 
-app.get('/students/:name', function(req, res){
-  res.send('My student name is ' +req.params.name);
+//GET, to request data from a document
+app.get('/provincie/:id', function(req, res){
+  const id = req.params.id; //var id opslaan
+  const user = users.filter(user => user.id === id)[0] // kijken of id overeenkomt met de id hierboven.
+  res.render('provincie.ejs', user);
 })
 
-app.post('/login', function(req, res){
-  console.log(req.body.email,req.body.password);
-  res.send('okayy');
+//GET, to request data from a document
+app.get('/geslacht/:id', function(req, res){
+  const id = req.params.id; //var id opslaan
+  const user = users.filter(user => user.id === id)[0] // kijken of id overeenkomt met de id hierboven.
+  res.render('man_vrouw.ejs', user);
 })
 
-//pug oefening, template engine
+//GET, to request data from a document
+app.get('/voornaam/:id', function(req, res){
+  const id = req.params.id; //var id opslaan
+  const user = users.filter(user => user.id === id)[0] // kijken of id overeenkomt met de id hierboven.
+  res.render('voornaam.ejs', user);
+})
+
+//GET, to request data from a document
+app.get('/afbeeldingen/:id', function(req, res){
+  const id = req.params.id; //var id opslaan
+  const user = users.filter(user => user.id === id)[0] // kijken of id overeenkomt met de id hierboven.
+  res.render('foto_toevoegen.ejs', user);
+})
+
+//GET, to request data from a document
+app.get('/tekst/:id', function(req, res){
+  const id = req.params.id; //var id opslaan
+  const user = users.filter(user => user.id === id)[0] // kijken of id overeenkomt met de id hierboven.
+  res.render('tekst_profiel.ejs', user);
+})
+
+//GET, to request data from a document
+app.get('/profiel/:id', function(req, res){
+  const id = req.params.id; //var id opslaan
+  const user = users.filter(user => user.id === id)[0] // kijken of id overeenkomt met de id hierboven.
+  res.render('mijn_profiel.ejs', user); // Laat dit ejs bestand zien
+})
+
+
+
+
+
+//POST, to send data from a document
+app.post('/aanmelden', addUser)
+
+//Functie die ingevulde  data terug gaat sturen naar de server en in de lege array 'user' stopt.
+function addUser(req, res){ //request, response
+
+  users.push({ // pushed onderstaande ingevulde data in de array 'users'
+    email: req.body.email,
+    id: req.body.userName,
+    password: req.body.password
+  })
+
+  console.log(req.body);  //Laat in de terminal de ingevulde gegevens zien.
+  res.redirect('voornaam/' + req.body.userName); //Geeft bestand 'voornaam.ejs' weer bij client, plus de voornaam die de gebruiker heeft ingevuld.
+  // Dit is de route!! Niet ejs bestand.
+}
+
+
+
+
+
+//POST, to send data from a document
+app.post('/voornaam', addFirstName)
+
+
+//Functie die ingevulde  data terug gaat sturen naar de server en in de array 'user' stopt. (komt niet bij de andere data??? vraag!)
+function addFirstName(req, res){ //request, response
+
+  const user = users.filter(user => user.id === req.body.id)[0] // kijken of id overeenkomt met de id hierboven.
+
+  user.firstName = req.body.firstName;
+
+  console.log(req.body); //Laat in de terminal de ingevulde gegevens zien.
+  res.redirect('geboortedatum/' + req.body.id); //Geeft bestand 'geboortedatum.ejs' weer bij client. Dit is de route!! Niet ejs bestand.
+}
+
+
+
+
+
+//POST, to send data from a document
+app.post('/geboortedatum', addDateOfBirth)
+
+
+//Functie die ingevulde  data terug gaat sturen naar de server en in de array 'user' stopt. (komt niet bij de andere data??? vraag!)
+function addDateOfBirth(req, res){ //request, response
+
+  const user = users.filter(user => user.id === req.body.id)[0] // kijken of id overeenkomt met de id hierboven.
+
+  user.dateOfBirth = req.body.dateOfBirth;
+
+  console.log(req.body); //Laat in de terminal de ingevulde gegevens zien.
+  res.redirect('provincie/' + req.body.id); //Geeft bestand 'provincie.ejs' weer bij client. Dit is de route!! Niet ejs bestand.
+}
+
+
+
+
+
+//POST, to send data from a document
+app.post('/provincie', addProvince)
+
+
+//Functie die ingevulde  data terug gaat sturen naar de server en in de array 'user' stopt. (komt niet bij de andere data??? vraag!)
+function addProvince(req, res){ //request, response
+
+
+  const user = users.filter(user => user.id === req.body.id)[0] // kijken of id overeenkomt met de id hierboven.
+
+  user.province = req.body.province;
+
+  console.log(req.body); //Laat in de terminal de ingevulde gegevens zien.
+  res.redirect('geslacht/' + req.body.id); //Geeft bestand 'man_vrouw.ejs' weer bij client. Dit is de route!! Niet ejs bestand.
+}
+
+
+
+
+
+
+//POST, to send data from a document
+app.post('/geslacht', addGender)
+
+
+//Functie die ingevulde  data terug gaat sturen naar de server en in de array 'user' stopt. (komt niet bij de andere data??? vraag!)
+function addGender(req, res){ //request, response
+
+  const user = users.filter(user => user.id === req.body.id)[0] // kijken of id overeenkomt met de id hierboven.
+
+  user.gender = req.body.gender;
+
+  console.log(req.body); //Laat in de terminal de ingevulde gegevens zien.
+  res.redirect('afbeeldingen/' + req.body.id); //Geeft bestand 'foto_toevoegen.ejs' weer bij client. Dit is de route!! Niet ejs bestand.
+}
+
+
+
+
+
+
+//POST, to send data from a document
+app.post('/afbeeldingen', addPictures)
+
+
+//Functie die ingevulde  data terug gaat sturen naar de server en in de array 'user' stopt. (komt niet bij de andere data??? vraag!)
+function addPictures(req, res){ //request, response
+
+  const user = users.filter(user => user.id === req.body.id)[0] // kijken of id overeenkomt met de id hierboven.
+
+  user.pictures = req.body.pictures;
+
+  console.log(req.body); //Laat in de terminal de ingevulde gegevens zien.
+  res.redirect('tekst/' + req.body.id); //Geeft bestand 'tekst_profiel.ejs' weer bij client. Dit is de route!! Niet ejs bestand.
+}
+
+
+
+
+
+//POST, to send data from a document
+app.post('/tekst', addText)
+
+
+//Functie die ingevulde  data terug gaat sturen naar de server en in de array 'user' stopt. (komt niet bij de andere data??? vraag!)
+function addText(req, res){ //request, response
+
+  const user = users.filter(user => user.id === req.body.id)[0] // kijken of id overeenkomt met de id hierboven.
+
+  user.textProfile = req.body.textProfile;
+
+  console.log(user);
+  console.log(req.body); //Laat in de terminal de ingevulde gegevens zien.
+  res.redirect('profiel/' + req.body.id); //Geeft bestand 'mijn_profiel.ejs' weer bij client. Dit is de route!! Niet ejs bestand.
+}
+
+
+
+
+/*//ejs oefening, template engine
 app.get('/index', function(req, res){
   res.render('index.ejs', {title: 'Hey', message: 'Hello there!'});
-})
-
-/*
-app.get('*', (req, res) => {
-  res.status(404).render('not-found.pug', {message: 'This is an error'});
 })
 */
 
